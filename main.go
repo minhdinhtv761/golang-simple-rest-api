159ccb7d1fbafeebf01432f69cf4f3a9413d6bb2
minhman727 <62245831+minhman727@users.noreply.github.com>
2023-02-22 12:41:59 +0700

    enhance: remove redundant code

--- main.go
+++ main.go
@@ -47,33 +47,3 @@ func runService(db *gorm.DB) error {
 
 	return r.Run()
 }
-
-//CREATE TABLE `restaurants` (
-//`id` int(11) NOT NULL AUTO_INCREMENT,
-//`owner_id` int(11) DEFAULT NULL,
-//`name` varchar(50) NOT NULL,
-//`addr` varchar(255) NOT NULL,
-//`city_id` int(11) DEFAULT NULL,
-//`lat` double DEFAULT NULL,
-//`lng` double DEFAULT NULL,
-//`cover` json DEFAULT NULL,
-//`logo` json DEFAULT NULL,
-//`shipping_fee_per_km` double DEFAULT '0',
-//`status` int(11) NOT NULL DEFAULT '1',
-//`created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-//`updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-//PRIMARY KEY (`id`),
-//KEY `owner_id` (`owner_id`) USING BTREE,
-//KEY `city_id` (`city_id`) USING BTREE,
-//KEY `status` (`status`) USING BTREE
-//) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-type Restaurant struct {
-	Id   int    `json:"id" gorm:"column:id;"`
-	Name string `json:"name" gorm:"column:name;"`
-	Addr string `json:"address" gorm:"column:addr;"`
-}
-
-func (Restaurant) TableName() string {
-	return "restaurants"
-}
--- modules/restaurant/model/restaurant.go
+++ modules/restaurant/model/restaurant.go
@@ -1,25 +1,5 @@
 package model
 
-//CREATE TABLE `restaurants` (
-//  `id` int(11) NOT NULL AUTO_INCREMENT,
-//  `owner_id` int(11) DEFAULT NULL,
-//  `name` varchar(50) NOT NULL,
-//  `addr` varchar(255) NOT NULL,
-//  `city_id` int(11) DEFAULT NULL,
-//  `lat` double DEFAULT NULL,
-//  `lng` double DEFAULT NULL,
-//  `cover` json DEFAULT NULL,
-//  `logo` json DEFAULT NULL,
-//  `shipping_fee_per_km` double DEFAULT '0',
-//  `status` int(11) NOT NULL DEFAULT '1',
-//  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-//  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-//  PRIMARY KEY (`id`),
-//  KEY `owner_id` (`owner_id`) USING BTREE,
-//  KEY `city_id` (`city_id`) USING BTREE,
-//  KEY `status` (`status`) USING BTREE
-//) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 type Restaurant struct {
 	Id   int    `json:"id,omitempty" gorm:"column:id;"`
 	Name string `json:"name" gorm:"column:name;"`
